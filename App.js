0e4dfab6be492842e6d0d89c71e6999323a2f486
duchieu1279 <tdhieu1279@gmail.com>
2022-11-20 21:23:56 +0700

    Sửa lỗi không ẩn được màn hình đăng nhập

--- App.js
+++ App.js
@@ -1,50 +1,12 @@
-import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import React from "react";
-import AudioProvider from "./app/context/AudioProvider";
-import { AppContext } from './app/context/AppProvider';
-import AppProvider from "./app/context/AppProvider";
-import color from "./app/misc/color";
-import AppNavigator from "./app/navigation/AppNavigator";
-import { useContext } from 'react';
-import { StatusBar } from 'react-native';
-import Login from './app/screens/Login';
-import Register from './app/screens/Register';
-
-const MyTheme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    background: color.APP_BG,
-  }
-}
+import AppProvider from './app/context/AppProvider'
+import Index from './index'
+import { registerRootComponent } from 'expo'
+import React from 'react'
 
 export default function App() {
-  const appContext = useContext(AppContext);
-  const Stack = createNativeStackNavigator();
-
   return (
     <AppProvider>
-      <AudioProvider>
-        <NavigationContainer theme = {MyTheme}>
-        <StatusBar showHideTransition="true" />
-          {!appContext ? (
-            <>
-              <Stack.Navigator
-                initialRouteName="Login"
-                screenOptions={{
-                  headerShown: false,
-                }}
-              >
-                <Stack.Screen name="Register" component={Register} />
-                <Stack.Screen name="Login" component={Login} />
-              </Stack.Navigator>
-            </>
-          ) : (
-            <AppNavigator />
-          )}
-        </NavigationContainer>
-      </AudioProvider>
+      <Index />
     </AppProvider>
-  );
+  )
 }
